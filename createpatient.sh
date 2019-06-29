a43ac1535c06abd7f957913250e4fe1ebfbef1a3
PaxPrz <paxprajapati@gmail.com>
2019-06-29 09:11:09 -0400

    search navbar rating sort

---
+++ createpatient.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+composer identity issue -c admin@drsewa -u $2 -a "resource:com.pax.drsewa.Patient#$1" -f $2.card
+sleep 1
+composer card import -f $2.card
+sleep 1
+composer-rest-server -c $2@drsewa -n never -u true -w true -p $3
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm *.card
-composer network install -c PeerAdmin@hlfv1 -a ./dists/drsewa@0.0.9.bna
+composer network install -c PeerAdmin@hlfv1 -a ./dists/drsewa@0.0.12.bna
+sleep 1
 composer network start -c PeerAdmin@hlfv1 -n drsewa -V 0.0.9 -A admin -S adminpw -f networkAdmin.card
 sleep 1
 composer card import -f networkAdmin.card
